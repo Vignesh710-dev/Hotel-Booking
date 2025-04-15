a47f6d8640541513ff7960ed530e322fbe3a71f9
Vignesh710-dev <vigneshvcoder@gmail.com>
2025-04-15 23:44:30 +0530

    Updated gitignore and server.js

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
+// Add this at the top after the requires
+const app = express();
+
 app.use(bodyParser.json());
 
 // Email configuration
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        user: process.env.EMAIL_USER, // Changed to use env variable
+        pass: process.env.EMAIL_PASS  // Changed to use env variable
     }
 });
 
@@ -60,3 +63,7 @@ app.post('/api/bookings', async (req, res) => {
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
+
+// Make sure your .env file has these variables:
+// EMAIL_USER=yourgmail@gmail.com
+// EMAIL_PASS=your_app_specific_password
